0ed34e2bbdc5b152f8250389cf8b21733d68d369
Jax Choong <choongjax@gmail.com>
2024-10-30 16:03:29 +0800

    feat: setup the piecs and their corresponding functions

--- pieces.cpp
+++ pieces.cpp
@@ -1,5 +1,7 @@
+#include "pieces.h"
+
 // remember this: u have 7 kinds of blocks, each with 4 rotations, each with 5x5 blocks
-char pieces[7 /*kinds */][4 /*rotations*/][5 /*width*/][5 /*height*/] = {
+char mPieces[7 /*kinds */][4 /*rotations*/][5 /*width*/][5 /*height*/] = {
     //square
     {
         {
@@ -218,3 +220,108 @@ char pieces[7 /*kinds */][4 /*rotations*/][5 /*width*/][5 /*height*/] = {
         }
     }
 };
+
+
+// Displacement of the piece to the position where it is first drawn in the board when it is created
+int mPiecesInitialPosition [7 /*kind */ ][4 /* r2otation */ ][2 /* position */] =
+{
+    /* Square */
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -3}
+    },
+    /* I */
+    {
+        {-2, -2},
+        {-2, -3},
+        {-2, -2},
+        {-2, -3}
+    },
+    /* L */
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -2}
+    },
+    /* L mirrored */
+    {
+        {-2, -3},
+        {-2, -2},
+        {-2, -3},
+        {-2, -3}
+    },
+    /* N */
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -2}
+    },
+    /* N mirrored */
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -2}
+    },
+    /* T */
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -2}
+    },
+};
+
+
+/*
+======================================
+Return the type of a block (0 = no-block, 1 = normal block, 2 = pivot block)
+
+Parameters:
+
+>> pPiece: Piece to draw
+>> pRotation: 1 of the 4 possible rotations
+>> pX: Horizontal position in blocks
+>> pY: Vertical position in blocks
+======================================
+*/
+int Pieces::GetBlockType (int pPiece, int pRotation, int pX, int pY)
+{
+    return mPieces [pPiece][pRotation][pX][pY];
+}
+
+/*
+======================================
+Returns the horizontal displacement of the piece that has to be applied in order to create it in the
+correct position.
+
+Parameters:
+
+>> pPiece: Piece to draw
+>> pRotation: 1 of the 4 possible rotations
+======================================
+*/
+int Pieces::GetXInitialPosition (int pPiece, int pRotation)
+{
+return mPiecesInitialPosition [pPiece][pRotation][0];
+}
+
+/*
+======================================
+Returns the vertical displacement of the piece that has to be applied in order to create it in the
+correct position.
+
+Parameters:
+
+>> pPiece: Piece to draw
+>> pRotation: 1 of the 4 possible rotations
+======================================
+*/
+int Pieces::GetYInitialPosition (int pPiece, int pRotation)
+{
+return mPiecesInitialPosition [pPiece][pRotation][1];
+}
